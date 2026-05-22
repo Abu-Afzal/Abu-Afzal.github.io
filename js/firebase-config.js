3caa58215273e04a4796adcb6742d9cbd6a833ff
Abu-Afzal <anthonioanthox@gmail.com>
2026-05-22 14:27:58 +0800

    Update firebase-config.js
    Signed-off-by: Abu-Afzal <anthonioanthox@gmail.com>

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyB24GCKSTPGlN9HG9E6uhCECVa4ibCpKEA",
   authDomain: "sipelita-digital.firebaseapp.com",
   projectId: "sipelita-digital",
-  storageBucket: "sipelita-digital.firebasestorage.app",
+  storageBucket: "sipelita-digital.appspot.com",
   messagingSenderId: "787840817745",
   appId: "1:787840817745:web:e6b5237cfbb5e51be93670",
   measurementId: "G-1D5DWJV54E"
