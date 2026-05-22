19f1e066ebf5c0b3918e0818e70f557d9188df57
Abu-Afzal <anthonioanthox@gmail.com>
2026-05-22 23:53:10 +0800

    Update firebase-config.js
    Signed-off-by: Abu-Afzal <anthonioanthox@gmail.com>

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,19 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
-// ⚠️ Ganti dengan config dari Firebase Console > Project Settings > General > Your apps
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB24GCKSTPGlN9HG9E6uhCECVa4ibCpKEA",
   authDomain: "sipelita-digital.firebaseapp.com",
+  databaseURL: "https://sipelita-digital-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "sipelita-digital",
-  storageBucket: "sipelita-digital.appspot.com",
+  storageBucket: "sipelita-digital.firebasestorage.app",
   messagingSenderId: "787840817745",
   appId: "1:787840817745:web:e6b5237cfbb5e51be93670",
   measurementId: "G-1D5DWJV54E"
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const db = getDatabase(app);
 export const storage = getStorage(app);
