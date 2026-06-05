c29af14070cbd383ac84e55fc5076e1e32dfa058
Abu-Afzal <anthonioanthox@gmail.com>
2026-06-05 21:41:22 +0800

    Update users.js
    Signed-off-by: Abu-Afzal <anthonioanthox@gmail.com>

--- users/users.js
+++ users/users.js
@@ -1,1 +1,8 @@
+import './fitur.js';
+import './load-users.js';
+import './tambah-user.js';
+import './edit-user.js';
+import './hapus-user.js';
+import './modal.js';
 
+console.log('✅ Users module loaded');
