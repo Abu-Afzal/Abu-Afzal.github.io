37ca6b1555cdcc94688a4cb25a4918d410e3e77f
Abu-Afzal <anthonioanthox@gmail.com>
2026-06-05 22:48:13 +0800

    Update firebase-config.js
    Signed-off-by: Abu-Afzal <anthonioanthox@gmail.com>

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,24 +1,52 @@
+```js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-// 1. TAMBAHKAN IMPORT FIRESTORE DI SINI
+
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+
+// FIREBASE CONFIG
 const firebaseConfig = {
+
   apiKey: "AIzaSyB24GCKSTPGlN9HG9E6uhCECVa4ibCpKEA",
+
   authDomain: "sipelita-digital.firebaseapp.com",
-  databaseURL: "https://sipelita-digital-default-rtdb.asia-southeast1.firebasedatabase.app",
+
+  databaseURL:
+  "https://sipelita-digital-default-rtdb.asia-southeast1.firebasedatabase.app",
+
   projectId: "sipelita-digital",
+
   storageBucket: "sipelita-digital.firebasestorage.app",
+
   messagingSenderId: "787840817745",
-  appId: "1:787840817745:web:e6b5237cfbb5e51be93670",
+
+  appId:
+  "1:787840817745:web:e6b5237cfbb5e51be93670",
+
   measurementId: "G-1D5DWJV54E"
 };
 
+
+// INITIALIZE APP
 const app = initializeApp(firebaseConfig);
 
-// 2. KITA EKSPOR DUA-DUANYA AGAR AMAN
-export const rtdb = getDatabase(app);      // RTDB lama tetap bisa diakses dengan nama rtdb jika dibutuhkan
-export const db = getFirestore(app);        // Variabel db SEKARANG MENJADI FIRESTORE
+
+// EXPORT SERVICES
+export const auth = getAuth(app);
+
+export const rtdb = getDatabase(app);
+
+export const db = getFirestore(app);
+
 export const storage = getStorage(app);
+
+
+// OPTIONAL
+export default app;
+```
