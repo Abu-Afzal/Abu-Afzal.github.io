f98f6bbb2d697f1f6a531c744dae83f27112f573
Abu-Afzal <anthonioanthox@gmail.com>
2026-06-06 22:39:33 +0800

    Add Google Drive API configuration
    Signed-off-by: Abu-Afzal <anthonioanthox@gmail.com>

---
+++ js/drive-config.js
@@ -0,0 +1,8 @@
+// Konfigurasi resmi Google Drive API v3
+export const DRIVE_CONFIG = {
+    // 1. Tempelkan API Key rahasia yang baru Anda buat di Google Cloud Console
+    API_KEY: "AIzaSyDTvl_73qc1hdVlmidAqQfmfPTWTRhBmKA", 
+    
+    // 2. Tempelkan ID Folder Induk/Utama Google Drive Anda
+    FOLDER_ID_UTAMA: "Galeri Kegiatan MAN Bantaeng" 
+};
