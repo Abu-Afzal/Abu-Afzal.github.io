d52f51ee16d68734b176bd17d3e63b87880784e4
Abu-Afzal <anthonioanthox@gmail.com>
2026-06-07 22:28:18 +0800

    Create sican-kegiatan.js
    Signed-off-by: Abu-Afzal <anthonioanthox@gmail.com>

---
+++ js/sican-kegiatan.js
@@ -0,0 +1,49 @@
+import {
+    db
+} from './firebase-config.js';
+
+import {
+    collection,
+    getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+export async function loadKegiatan(){
+
+    const select =
+        document.getElementById('kegiatanSelect');
+
+    select.innerHTML =
+        '<option value="">Pilih Kegiatan</option>';
+
+    try{
+
+        const snapshot =
+            await getDocs(
+                collection(db, 'kegiatan_pembiasaan')
+            );
+
+        snapshot.forEach(doc => {
+
+            const data = doc.data();
+
+            select.innerHTML += `
+
+                <option value="${data.nama}">
+                    ${data.nama}
+                </option>
+
+            `;
+        });
+
+    }
+    catch(err){
+
+        console.error(
+            'Gagal memuat kegiatan:',
+            err
+        );
+
+        select.innerHTML =
+            '<option>Gagal memuat</option>';
+    }
+}
