8b4ac85daeb827240296a3e08a9a09d4e60eef9c
Abu-Afzal <anthonioanthox@gmail.com>
2026-06-16 06:24:44 +0800

    Update config.js
    Signed-off-by: Abu-Afzal <anthonioanthox@gmail.com>

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ const CONFIG = {
             id: 'pusaka', 
             title: 'Absensi Pusaka', 
             desc: '', 
-            color: '#283593', 
+            color: '#ebebeb', 
             url: 'https://pusaka-v3.kemenag.go.id/',
             logo: 'assets/images/icon-app.png'
         },
